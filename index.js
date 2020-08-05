09847b54ab85615ae2d3a6983524f4aa56d5ea94
Ricky Rhodes <ricky@rickyrhodes.com>
2020-08-05 19:33:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  const lis = document.querySelectorAll('ul.ranked-list li')
+  for (let j = 0; j < lis.length; j++) {
+    lis[j].innerHTML = parseFloat(lis[j].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  let parent = document.querySelector('div#grand-node').querySelectorAll('div')
+  
+  for (let i = 0; i < parent.length; i++) {
+  if (i === (parent.length - 1)) {
+    return parent[i]
+    }
+  }
+}
